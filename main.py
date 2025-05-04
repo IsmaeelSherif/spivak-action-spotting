d436d3f3f409630a4437af78d4605fb0d893f4f5
ismaeel <ismaeelsherif2002@ymail.com>
2025-05-04 17:13:11 +0300

    _init_

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-import os
-import sys
-# Get the directory containing the current script (main.py)
-current_dir = os.path.dirname(os.path.abspath(__file__))
-# Get the parent directory (spivak-action-spotting)
-parent_dir = os.path.dirname(current_dir) + '/spivak-action-spotting'
-# Add the parent directory to sys.path
-sys.path.append(parent_dir)
-print('added to sys.path', parent_dir)
-
 
 from bin.spotting_challenge_commands import \
     commands_spotting_challenge, print_commands, BAIDU_TWO_FEATURE_NAME, \
