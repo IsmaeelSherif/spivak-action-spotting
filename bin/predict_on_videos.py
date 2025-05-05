3b52d56f9320b883eb1f64cda662e5cd3ecb3da7
ismaeel <ismaeelsherif2002@ymail.com>
2025-05-05 21:08:33 +0300

    path

--- bin/predict_on_videos.py
+++ bin/predict_on_videos.py
@@ -4,6 +4,20 @@
 # Licensed under the Apache License, Version 2.0.
 # See the accompanying LICENSE file for terms.
 
+
+import sys
+import os
+
+# Get the absolute path to the parent directory of 'eztorch'
+eztorch_parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+# Add it to the Python path if it's not already there
+if eztorch_parent_dir not in sys.path:
+    print('added to path')
+    sys.path.insert(0, eztorch_parent_dir)
+
+    
+
 import argparse
 import logging
 from pathlib import Path
