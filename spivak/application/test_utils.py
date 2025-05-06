4fb0d0df0b1b772ab3171eaf6525c27644692247
ismaeel <ismaeelsherif2002@ymail.com>
2025-05-06 11:03:42 +0300

    changed feature_name

--- spivak/application/test_utils.py
+++ spivak/application/test_utils.py
@@ -60,7 +60,7 @@ def test(args: SharedArgs) -> None:
     except:
         print('1 args.feature_name', 'None')
 
-    args.feature_name = "ResNET_TF2"
+    args.feature_name = "ResNET_TF2_PCA512"
     dataset = create_dataset(args, split, label_maps)
     results_dir = Path(args.results_dir)
     results_dir.mkdir(exist_ok=True)
