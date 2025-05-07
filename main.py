78a2443cddf2606fb38fbad6202cc57c4426cb8c
ismaeel <ismaeelsherif2002@ymail.com>
2025-05-07 19:16:13 +0300

    print command on resnet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from bin.spotting_challenge_commands import \
-    commands_spotting_challenge, print_commands, BAIDU_TWO_FEATURE_NAME, \
+    commands_spotting_challenge, commands_spotting_challenge_validated, RESNET_NORMALIZED_FEATURE_NAME, RESNET_NORMALIZED_FEATURES_DIR, print_commands, BAIDU_TWO_FEATURE_NAME, \
     BAIDU_TWO_FEATURES_DIR, RUN_NAME_ZOO
 
 
@@ -10,3 +10,11 @@ baidu_two_challenge_commands_zoo = commands_spotting_challenge(
     run_name=RUN_NAME_ZOO, do_train=False)
 
 print_commands(baidu_two_challenge_commands_zoo)
+
+
+# Commands for running just testing with the models from the zoo:
+resnet_normalized_challenge_validated_commands_zoo = \
+    commands_spotting_challenge_validated(
+        RESNET_NORMALIZED_FEATURES_DIR, RESNET_NORMALIZED_FEATURE_NAME,
+        run_name=RUN_NAME_ZOO, do_train=False)
+print_commands(resnet_normalized_challenge_validated_commands_zoo)
